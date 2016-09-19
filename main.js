3cae5d4f7ee122343d4c3a3d464e7e6ad16a0674
Amidou Kante <kamdiou@gmail.com>
2016-09-18 23:26:35 -0400

    Bug fixes

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
 var latitude = 0;
 var longitude = 0;
 var city = "";
+var temp = 0;
+var tempC = 0;
 	
 function getLocation(){  
-	$.ajax({
-		type: "GET",
-		url: "http://ip-api.com/json/?callback=?",
-		jsonpCallback: "callback",
-		dataType: 'jsonp',
-		success: function(json) {
-			latitude = json.lat;
-			longitude = json.lon;
-			city = json.city;
-			WeatherApp();
-		}
+	$get("http://freegeoip.net/json/", 
+	function(json){
+		latitude = json.latitude;
+		longitude = json.longitude;
+		city = json.region_name;
+		WeatherApp();
 	});
-}
-	
+};	
 
 function WeatherApp(){
 	$.get("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&mode=JSON&units=imperial&appid=9815c2fc2b5f1abd1a9ced35afe57d77", 
@@ -24,16 +20,29 @@ function WeatherApp(){
 		data = json;
 		var status = "http://openweathermap.org/img/w/"+data.weather[0].icon+".png";
 		var description = data.weather[0].description;
-		$(".currentLocation").html(data.name);
+		var temp = Math.floor(data.main.temp);
+		$(".currentLocation").html(json.region_name);
 		$(".currentCondition").html(data.weather[0].main);
 		$(".imgCondition").attr("src",status);
 		$(".imgCondition").attr("title",description);
-		$(".currentTemp").html(data.main.temp + '&deg F');
+		$(".currentTemp").html(temp + '&deg F');
 		$('.tempMin').html('Min: '+data.main.temp_min+"&deg F ");
 		$('.tempMax').html('Max: '+data.main.temp_max+"&deg F ");
 		$('.humidity').html('Humidity: '+data.main.humidity+"%");
 	});
+	$('.currentTemp').on('click',function(){
+		$(this).hide();
+		$('.currentTempC').show();
+		$('.currentTempC').html(Math.floor((parseInt(temp) - 32) * 5/9) + "&deg F ");
+		return false;
+	});
+	$('.currentTempC').on('click', function(){
+		$(this).hide();
+		$('.currentTemp').show();
+		$('.currentTemp').html(Math.floor(data.main.temp)+"&deg F ");
+	});
 };
+
 
 $(function() {
   WeatherApp();
